47e0bbdefe58d1d81a2d881f32f2c1fd9acb0667
dolapelao <crist.castrobuocuc.cl>
2024-05-17 22:10:27 -0400

    commit

---
+++ app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import index, contact, destination, pricing
+
+urlpatterns = [
+    path('app/index/',index, name='index'),
+    path('', index, name='index'),
+    path('app/contact/', contact, name='contact'),
+    path('app/destination/', destination, name='destination'),
+    path('app/pricing/', pricing, name='pricing'),    
+]
+
+
--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render (request, "app/index.html")
+
+def contact(request):
+    return render (request,"app/contact.html")
+
+def destination(request):
+    return render (request,"app/destination.html")
+
+def pricing(request):
+    return render (request,"app/pricing.html")
